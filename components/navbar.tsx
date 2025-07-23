b560270ee2f19d3770daef401c194631a2c1a988
prasadkadam03 <prasadkadam29503@gmail.com>
2025-07-23 16:48:22 +0530

    Implemented NavBar

---
+++ components/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./navitems";
+const Navbar = () => {
+
+    return (
+        <div className="flex items-center justify-between mx-auto w-full px-14 py-4 bg-white max-sm:px-4">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image src="images/logo.svg" alt="Logo" width={40} height={40} />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <p>Sign In</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
